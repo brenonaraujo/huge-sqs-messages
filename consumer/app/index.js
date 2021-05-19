a122c69afaa8c59a11a949933347b69ad97c355f
Brenon Araujo <brenonaraujo@Brenons-MacBook-Pro.local>
2021-05-19 07:39:51 -0300

    setup from repository and start to create the form service

--- consumer/app/index.js
+++ consumer/app/index.js
@@ -1,9 +1,30 @@
-import { SqsConsumer } from 'sns-sqs-big-payload';
+import { SqsConsumer, SqsConsumerEvents } from 'sns-sqs-big-payload';
+
+const sqsConsumer = SqsConsumer.create({
+    region: 'us-east-1',
+    getPayloadFromS3: true,
+    s3Bucket: '...',
+
+    transformMessageBody: (body) => {
+        const snsMessage = JSON.parse(body);
+        return snsMessage.Message;
+    },
+    parsePayload: (raw) => JSON.parse(raw),
+    handleMessage: async ({ payload }) => {
+        // ...
+    },
+});
 
 exports.handler =  async (event, context) => {
-    return {
-        statusCode: 200,
-        body: `Response to event: ${event}`,
-        isBase64Encoded: false
-    }
+    await Promise.all(
+        event.Records.map(async (record) => {
+            try {
+                await sqsConsumer.processMessage(record, { deleteAfterProcessing: false });
+            } catch (error) {
+                console.error(error);
+                return error;
+            }
+        })
+    );
+    return {}
 }
