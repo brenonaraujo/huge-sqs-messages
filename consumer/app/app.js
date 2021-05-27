75c0ccc651478599cd91ee83afefd7020d2b2ed5
Brenon Araujo <brenonaraujo@Brenons-MacBook-Pro.local>
2021-05-26 22:37:10 -0300

    removed modules

--- consumer/app/app.js
+++ consumer/app/app.js
@@ -1,14 +1,14 @@
+console.log(`Form Consumer Lambda Starting...`);
 const { SqsConsumer } = require('sns-sqs-big-payload');
 const Dynamoose = require('dynamoose');
 const FormService = require('./src/services/form.service');
+console.log(`Libraries loaded...`);
 
 const formService = new FormService(Dynamoose);
-
 const sqsConsumer = SqsConsumer.create({
     region: 'us-east-1',
     getPayloadFromS3: true,
-    s3Bucket: '...',
-
+    s3Bucket: 'sqs-huge-messages',
     transformMessageBody: (body) => {
         const snsMessage = JSON.parse(body);
         return snsMessage.Message;
@@ -19,12 +19,16 @@ const sqsConsumer = SqsConsumer.create({
         return await formService.formPersist(payload);
     },
 });
+console.log(`Consumer service created...`);
 
 exports.handler = async (event, context) => {
+    console.log(`Event: ${JSON.stringify(event)}`);
+    console.log(`Message/s received.`);
     await Promise.all(
         event.Records.map(async (record) => {
             try {
-               return await sqsConsumer.processMessage(record, { deleteAfterProcessing: false });
+                console.log(`Starting to process message.`)
+                return await sqsConsumer.processMessage(record, { deleteAfterProcessing: false });
             } catch (error) {
                 console.error(error);
                 return error;
