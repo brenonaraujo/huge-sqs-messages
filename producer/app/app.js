eb9cf0408159c84c8cad8a2fd799cefa1521f1c7
brenonaraujo <brenonaraujo@gmail.com>
2021-05-27 09:36:25 -0300

    performance observer with marker measure added

--- producer/app/app.js
+++ producer/app/app.js
@@ -1,5 +1,7 @@
-const baseMessage = require('./resources/affordable.mock.json');
+const affodableMessage = require('./resources/affordable.mock.json');
+const hugeMessage = require('./resources/huge.mock.json');
 const { SqsProducer } = require('sns-sqs-big-payload');
+const { PerformanceObserver, performance } = require('perf_hooks'); 
 
 const formConsumerQueue = SqsProducer.create({
     queueUrl: 'https://sqs.us-east-1.amazonaws.com/736735782839/form-consumer-queue',
@@ -9,22 +11,33 @@ const formConsumerQueue = SqsProducer.create({
     s3Bucket: 'sqs-huge-messages',
 });
 
-async function messageProducer() {
+async function messageProducerApp() {
     console.log(`Start to produce messages`);
-    for (let index = 0; index < process.env.MESSAGES_TO_PRODUCE | 100; index++) {
-        baseMessage.index = index;
+    for (let index = 0; index < 1; index++) {
         try {
-            await formConsumerQueue.sendJSON(baseMessage);
+            performance.mark(`setupIndex[${index}]`);
+            affodableMessage.index = index;
+            performance.measure(`setupIndex[${index}] to now`, `setupIndex[${index}]`);
+            performance.mark(`sendingMessage[${index}]`);
+            await formConsumerQueue.sendJSON(affodableMessage); // TEMP
             console.log(`Message with index: ${index} sended to the queue!`);
+            performance.measure(`sendingMessage[${index}] to finish`, `sendingMessage[${index}]`);
         } catch (error) {
             console.error(error);
             break;
         }
-
     }
-    console.log(`finish!!!`);
 }
 
-messageProducer();
+const obs = new PerformanceObserver((items) => {
+    items.getEntries().forEach(item => {
+        console.log(`(${item.name}) - Time elapsed: ${item.duration}`);
+    })
+  });
+obs.observe({ type: 'measure' });
+
+messageProducerApp();
+
+
 
 
