3cef42a03da24a65100555abdf58a693891b3da7
brenonaraujo <brenonaraujo@gmail.com>
2021-05-29 18:20:32 -0300

    Some producer fixes and added more info into the measurements log

--- producer/app/app.js
+++ producer/app/app.js
@@ -1,7 +1,8 @@
-const messagePayload = require('./resources/normal.mock.json');
-const { SqsProducer } = require('sns-sqs-big-payload');
 const { PerformanceObserver, performance } = require('perf_hooks');
 const uuid = require('uuid');
+const messagePayload = require('./resources/huge.mock.json');
+const { SqsProducer } = require('sns-sqs-big-payload');
+const EventEmitter = require('events');
 
 const formConsumerQueue = SqsProducer.create({
     queueUrl: 'https://sqs.us-east-1.amazonaws.com/736735782839/form-consumer-queue',
@@ -11,27 +12,48 @@ const formConsumerQueue = SqsProducer.create({
     s3Bucket: 'sqs-huge-messages',
 });
 
+const numberOfMessagesToSend = 1;
+
 async function messageProducerApp() {
     console.log(`Start to produce messages`);
-    for (let index = 0; index < 100; index++) {
+    const messagesToSend = [];
+    const buildMessage = (messagePayload) => formConsumerQueue.sendJSON(messagePayload);
+    for (let index = 0; index < numberOfMessagesToSend; index++) {
         try {
             performance.mark(`setupIndex[${index}]`);
-            messagePayload._id = uuid.v4();
+            messagePayload._id = new uuid.v4();
             performance.measure(`setupIndex[${index}] to now`, `setupIndex[${index}]`);
-            performance.mark(`sendingMessage[${index}]`);
-            await formConsumerQueue.sendJSON(messagePayload); // TEMP
+            performance.mark(`buildingMessage[${index}]`);
+            messagesToSend.push(buildMessage(messagePayload));
             console.log(`Message with index: ${index} sended to the queue!`);
-            performance.measure(`sendingMessage[${index}] to finish`, `sendingMessage[${index}]`);
+            performance.measure(`buildingMessage[${index}] to send`, `buildingMessage[${index}]`);
         } catch (error) {
             console.error(error);
             break;
         }
     }
+    performance.mark(`sendAllMessages`);
+    Promise.all(messagesToSend)
+        .then(() => {
+            console.log(`All messages sended!`);
+            performance.measure(`sendAllMessages to finish`, `sendAllMessages`);
+        })
 }
+
+EventEmitter.on('', () => {
+
+});
 
 const obs = new PerformanceObserver((items) => {
     items.getEntries().forEach(item => {
-        console.log(`(${item.name}) - Time elapsed: ${item.duration.toFixed(3)} ms`);
+        const sendAllElapsed = item.duration.toFixed(3);
+        console.log(`(${item.name}) - Time elapsed: ${sendAllElapsed} ms`);
+        if (item.name === 'sendAllMessages to finish') {
+            console.log(`
+            Messages sent: ${numberOfMessagesToSend},
+            Messages sent per sec: ${((numberOfMessagesToSend/sendAllElapsed)*1000)} Msg/s,
+            `);
+        }
     })
 });
 obs.observe({ type: 'measure' });
